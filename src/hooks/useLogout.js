f54c07906efeb27e3432dc9812366631d64fb853
Daniel O'Brien <daniel@monomania.ie>
2021-12-09 10:32:32 +0000

    logout created

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,15 @@
+// firebse imports
+import { auth } from '../firebase/config';
+import { signOut } from '@firebase/auth';
+
+export const useLogout = () => {
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('user signed out');
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  return { logout };
+};
