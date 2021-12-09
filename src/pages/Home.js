e8ab609b9ee8cc3e3930f7abe48c4dc08483f018
Daniel O'Brien <daniel@monomania.ie>
2021-12-09 09:41:26 +0000

    changed db connection to real time listener

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useCollection } from '../hooks/useCollection';
+
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
-import { db } from '../firebase/config';
-import { collection, getDocs } from 'firebase/firestore';
 
 export default function Home() {
-  const [books, setBooks] = useState(null);
-
-  useEffect(() => {
-    const ref = collection(db, 'books');
-    getDocs(ref).then((snapshot) => {
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({ id: doc.id, ...doc.data() });
-      });
-      setBooks(results);
-    });
-  }, []);
+  const { documents: books } = useCollection('books');
 
   return (
     <div>
